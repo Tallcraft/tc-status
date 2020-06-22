ebd6e7c64cb9f5b7c31aff67586b0e012827334f
Trikolon <git@pbz.im>
2020-06-22 22:44:24 +0200

    Adapt to system theme (with reactivity).

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,16 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
 
+const mq = window.matchMedia('(prefers-color-scheme: dark)');
+
 Vue.use(Vuetify);
 
-export default new Vuetify({
+const vuetify = new Vuetify({
+  theme: { dark: mq.matches },
 });
+
+mq.addEventListener('change', (e) => {
+  vuetify.framework.theme.dark = e.matches;
+});
+
+export default vuetify;
