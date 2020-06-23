e50954dacb3d193f5ba46a3fe97140c349cbef30
Trikolon <git@pbz.im>
2020-06-23 21:53:53 +0200

    Don't fail if watchMedia event listener is not supported.

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,15 +1,16 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
 
-const mq = window.matchMedia('(prefers-color-scheme: dark)');
+const mq = window.matchMedia?.('(prefers-color-scheme: dark)');
 
 Vue.use(Vuetify);
 
 const vuetify = new Vuetify({
-  theme: { dark: mq.matches },
+  theme: { dark: mq?.matches },
 });
 
-mq.addEventListener('change', (e) => {
+// eslint-disable-next-line no-unused-expressions
+mq?.addEventListener?.('change', (e) => {
   vuetify.framework.theme.dark = e.matches;
 });
 
